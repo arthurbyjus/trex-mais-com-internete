9db7384912a1a1609e8805c8d51bb0a5d05c473b
arthurbyjus <95028998+arthurbyjus@users.noreply.github.com>
2021-12-01 15:51:31 -0300

    1/12/2021

--- sketch.js
+++ sketch.js
@@ -2,10 +2,13 @@
 var trex, trexCorrendo;
 var solo,imagem;
 var inv;
+var nuvem;
+var nuvemm;
 function preload(){
 //Carrega as imagens para a animação do T-rex
 trexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
-imagem=loadImage("ground2.png")
+imagem=loadImage("ground2.png"); 
+nuvemm=loadImage("cloud.png");
 }
 
 function setup(){
@@ -22,11 +25,14 @@ borda = createEdgeSprites();
 
 inv=createSprite(200,390,400,10);
 inv.visible=false
+
+//var numero = Math.round(random(1,50));
+//console.log(numero);
 }
 function draw(){
 //pinta o fundo da tela do jogo de branco
 background("black");
-console.log(trex.y);     
+//console.log(trex.y);     
 solo.velocityX=-2;
 if(solo.x<0){
 solo.x=solo.width/2;
@@ -39,6 +45,20 @@ trex.velocityY = -13;
 trex.velocityY = trex.velocityY + 1;
 //Impede que o T-rex caia da tela
 trex.collide(inv);
+//chamar uma função que desenha as nuvens
+gerarNuvens();
 //Desenha todos os sprites
 drawSprites();
+}
+
+function gerarNuvens(){
+if(frameCount%119   ===0){
+    nuvem=createSprite(1200,100,40,10) ;
+    nuvem.addImage(nuvemm);
+    nuvem.y=Math.round(random(10,200));
+    nuvem.velocityX=-3;  
+    nuvem.depth=trex.depth;
+    trex.depth=trex.depth+1
+}
+ 
 }
